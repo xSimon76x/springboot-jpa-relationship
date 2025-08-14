f9d0628d80e83e48b986ec1c1c0595204e2f2618
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-14 16:25:54 -0400

    Codificando ejemplo ManyToOne

--- src/main/java/com/simon/springboot/jpa/relationship/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/simon/springboot/jpa/relationship/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,42 @@
 package com.simon.springboot.jpa.relationship.springboot_jpa_relationship;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.simon.springboot.jpa.relationship.springboot_jpa_relationship.entities.Client;
+import com.simon.springboot.jpa.relationship.springboot_jpa_relationship.entities.Invoice;
+import com.simon.springboot.jpa.relationship.springboot_jpa_relationship.repositories.ClientRepository;
+import com.simon.springboot.jpa.relationship.springboot_jpa_relationship.repositories.InvoiceRepository;
+
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		manyToOne();
+	}
+
+	public void manyToOne() {
+		Client client = new Client("John", "Doe");
+		Invoice invoice = new Invoice("compras de oficina", 2000L);
+		invoice.setClient(client);
+
+		clientRepository.save(client);
+		Invoice invoiceDB = invoiceRepository.save(invoice);
+
+		System.out.println(invoiceDB);
 	}
 
 }
--- src/main/java/com/simon/springboot/jpa/relationship/springboot_jpa_relationship/entities/Invoice.java
+++ src/main/java/com/simon/springboot/jpa/relationship/springboot_jpa_relationship/entities/Invoice.java
@@ -42,6 +42,14 @@ public class Invoice {
         this.total = total;
     }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
     @Override
     public String toString() {
         return "{id=" + id + ", description=" + description + ", total=" + total + ", client=" + client + "}";
